6c4293006e50408e5709a45e532f0b5accae1cd0
W2H Galaxy <82048990+W2HGalaxy-OP@users.noreply.github.com>
2021-09-07 15:12:13 +0000

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -3,3 +3,8 @@ from userbot.utils import *
 from userbot.Config import Config
 from userbot.helpers.functions import *
 from userbot.cmdhelp import CmdHelp
+
+
+LEGEND_USER = bot.me.first_name
+Legend_Mr_Hacker = bot.uid
+w2h_mention = f"[{LEGEND_USER}](tg://user?id={Legend_Mr_Hacker})"
