8d79278c6467bc1c13ccca92c59da70f31cc5f9a
W2H Galaxy <82048990+W2HGalaxy-OP@users.noreply.github.com>
2022-02-05 14:47:46 +0530

    Update logos.py

--- userbot/plugins/logos.py
+++ userbot/plugins/logos.py
@@ -1,94 +1,103 @@
+# Ultroid - UserBot
+# Copyright (C) 2021-2022 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+"""
+✘ Commands Available -
+• `{i}logo <text>`
+   Generate a logo of the given Text
+   Or Reply To image , to write ur text on it.
+   Or Reply To Font File, To write with that font.
+"""
+import glob
 import os
 import random
-import time
 
-from PIL import Image, ImageDraw, ImageFont
-from telethon.tl.types import InputMessagesFilterPhotos, InputMessagesFilterDocument
+from pyUltroid.functions.misc import unsplashsearch
+from pyUltroid.functions.tools import make_logo
+from telethon.tl.types import InputMessagesFilterPhotos
 
-from userbot import *
-from W2HBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
-from telethon import events
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import CMD_HELP
-from userbot import ALIVE_NAME
+from . import OWNER_ID, OWNER_NAME, download_file, get_string, mediainfo, ultroid_cmd
 
-PICS_STR = []
 
-@bot.on(admin_cmd(pattern=r"logo ?(.*)"))
-@bot.on(sudo_cmd(pattern=r"logo ?(.*)", allow_sudo=True))
-async def lg1(W2Hevent):
-    event = await edit_or_reply(W2Hevent, "`ban raha hai tumhara logo.....`")
-    fnt = await get_font_file(event.client, "@W2h_fonts")
-    if W2Hevent.reply_to_msg_id:
-        rply = await W2Hevent.get_reply_message()
-        logo_ = await rply.download_media()
+@ultroid_cmd(pattern="logo ?(.*)")
+async def logo_gen(event):
+    xx = await event.eor(get_string("com_1"))
+    name = event.pattern_match.group(1)
+    if not name:
+        await xx.eor("`Give a name too!`", time=5)
+    bg_, font_ = None, None
+    if event.reply_to_msg_id:
+        temp = await event.get_reply_message()
+        if temp.media:
+            if hasattr(temp.media, "document") and (
+                ("font" in temp.file.mime_type)
+                or (".ttf" in temp.file.name)
+                or (".otf" in temp.file.name)
+            ):
+                font_ = await temp.download_media("resources/fonts/")
+            elif "pic" in mediainfo(temp.media):
+                bg_ = await temp.download_media()
+    if not bg_:
+        if event.client._bot:
+            SRCH = ["blur background", "background", "neon lights", "wallpaper"]
+            res = await unsplashsearch(random.choice(SRCH), limit=1)
+            bg_ = await download_file(res[0], "resources/downloads/logo.png")
+        else:
+            pics = []
+            async for i in event.client.iter_messages(
+                "@UltroidLogos", filter=InputMessagesFilterPhotos
+            ):
+                pics.append(i)
+            id_ = random.choice(pics)
+            bg_ = await id_.download_media()
+
+    if not font_:
+        fpath_ = glob.glob("resources/fonts/*")
+        font_ = random.choice(fpath_)
+    if len(name) <= 8:
+        strke = 10
+    elif len(name) >= 9:
+        strke = 5
     else:
-        async for i in bot.iter_messages("@w2h_logos", filter=InputMessagesFilterPhotos):
-    	    PICS_STR.append(i)
-        pic = random.choice(PICS_STR)
-        logo_ = await pic.download_media()
-    text = W2Hevent.pattern_match.group(1)
-    if len(text) <= 8:
-        font_size_ = 150
-        strik = 10
-    elif len(text) >= 9:
-        font_size_ = 50
-        strik = 5
-    else:
-        font_size_ = 130
-        strik = 20
-    if not text:
-        await eod(event, "**Give some text to make a logo !!**")
-        return
-    img = Image.open(logo_)
+        strke = 20
+    make_logo(
+        bg_,
+        name,
+        font_,
+        fill="white",
+        stroke_width=strke,
+        stroke_fill="black",
+    )
+    """img = Image.open(bg_)
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(fnt, font_size_)
-    image_widthz, image_heightz = img.size
-    w, h = draw.textsize(text, font=font)
+    font = ImageFont.truetype(font_, fnt_size)
+    w, h = draw.textsize(name, font=font)
     h += int(h * 0.21)
     image_width, image_height = img.size
     draw.text(
         ((image_width - w) / 2, (image_height - h) / 2),
-        text,
+        name,
         font=font,
         fill=(255, 255, 255),
     )
-    w_ = (image_width - w) / 2
-    h_ = (image_height - h) / 2
+    x = (image_width - w) / 2
+    y = (image_height - h) / 2
     draw.text(
-        (w_, h_), text, font=font, fill="white", stroke_width=strik, stroke_fill="black"
-    )
-    file_name = "W2HBOT.png"
-    img.save(file_name, "png")
-    await bot.send_file(
-        W2Hevent.chat_id,
-        file_name,
-        caption=f"**Made By :** {ALIVE_NAME}",
-    )
-    await event.delete()
-    try:
-        os.remove(file_name)
-        os.remove(fnt)
-        os.remove(logo_)
-    except:
-    	pass
-
-
-async def get_font_file(client, channel_id):
-    font_file_message_s = await client.get_messages(
-        entity=channel_id,
-        filter=InputMessagesFilterDocument,
-        limit=None,
-    )
-    font_file_message = random.choice(font_file_message_s)
-
-    return await client.download_media(font_file_message)
-
-
-CmdHelp("logos").add_command(
-  "logo", "<reply to pic + text> or <text>", "Makes a logo with the given text. If replied to a picture makes logo on that else gets random BG."
-).add_info(
-  "Logo Maker.\n**🙋🏻‍♂️ Note :**  Currently only supports custom pics. Fonts are choosen randomly."
-).add_warning(
-  "✅ Harmless Module."
-).add()
+        (x, y), name, font=font, fill="white", stroke_width=strke, stroke_fill="black"
+    )"""
+    flnme = "Logo.png"
+    await xx.edit("`Done!`")
+    if os.path.exists(flnme):
+        await event.client.send_file(
+            event.chat_id,
+            file=flnme,
+            caption=f"Logo by [{OWNER_NAME}](tg://user?id={OWNER_ID})",
+            force_document=True,
+        )
+        os.remove(flnme)
+        await xx.delete()
+    if os.path.exists(bg_):
+        os.remove(bg_)
