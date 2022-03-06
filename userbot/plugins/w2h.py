e8424f730517d11d57c176bf86b05595ab120632
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-03-06 23:59:28 +0530

    Update w2h.py

--- userbot/plugins/w2h.py
+++ userbot/plugins/w2h.py
@@ -3,7 +3,7 @@ import time
 from telethon import version
 from userbot import ALIVE_NAME, StartTime, W2Hversion
 from W2HBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
-
+from userbot.Config import Config
 
 async def reply_id(event):
     reply_to_id = None
@@ -57,23 +57,12 @@ uptime = get_readable_time((time.time() - StartTime))
 @bot.on(admin_cmd(outgoing=True, pattern="w2h$"))
 @bot.on(sudo_cmd(pattern="w2h$", allow_sudo=True))
 async def amireallyalive(alive):
-    if alive.fwd_from:
-        return
-    reply_to_id = await reply_id(alive)
-
-    if W2H_IMG:
-        W2H_caption = f"**{CUSTOM_ALIVE_TEXT}**\n"
-        W2H_caption += f"~~~~~~~~~~~~~~~~~~~~~~~\n"
-        W2H_caption += f"     __**BOT STATUS**__\n\n"
-        W2H_caption += f"**★ Telethon version :** `{version.__version__}`\n"
-        W2H_caption += f"**★ W2HBOT :**`{W2Hversion}`\n"
-        W2H_caption += f"**★ Uptime :** `{uptime}\n`"
-        W2H_caption += f"**★ Master:** {mention}\n"
-        await alive.client.send_file(
-            alive.chat_id, W2H_IMG, caption=W2H_caption, reply_to=reply_to_id
-        )
-        await alive.delete()
-    else:
+    try:
+        legend = await bot.inline_query(Config.BOT_USERNAME, "alive")
+        await legend[0].click(event.chat_id)
+        if event.sender_id == USERID:
+            await event.delete()
+    except (noin, dedbot):
         await edit_or_reply(
             alive,
             f"**{CUSTOM_ALIVE_TEXT}**\n\n"
