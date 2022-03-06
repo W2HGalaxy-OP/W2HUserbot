50ac7b3fa354e094c7845b8d011d0166fb82a4d3
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-03-06 17:01:04 +0530

    Update broadcast.py

--- userbot/plugins/broadcast.py
+++ userbot/plugins/broadcast.py
@@ -8,51 +8,66 @@ from . import *
 
 @bot.on(admin_cmd(pattern="gcast ?(.*)"))
 async def gcast(event):
-    if not event.out and not is_fullsudo(event.sender_id):
-        return await edit_or_reply(event, "`This Command Is Sudo Restricted.`")
-    xx = event.pattern_match.group(1)
-    if not xx:
-        return edit_or_reply(event, "`Give some text to Globally Broadcast`")
-    tt = event.text
-    msg = tt[6:]
-    event = await edit_or_reply(event, "`Globally Broadcasting Msg...`")
-    er = 0
-    done = 0
-    async for x in bot.iter_dialogs():
-        if x.is_group:
-            chat = x.id
+    reply_msg = await event.get_reply_message()
+    type = event.text[7:9]
+    if reply_msg:
+        tol = reply_msg.text
+        file = reply_msg.media
+    else:
+        tol = event.text[9:]
+        file = None
+    if tol == "":
+        return await eod(event, "I need something to Gcast.")
+    hol = await eor(event, "Gcasting message...")
+    sed = 0
+    lol = 0
+    if type == "-a":
+        async for aman in event.client.iter_dialogs():
+            chat = aman.id
             try:
-                done += 1
-                await bot.send_message(chat, msg)
+                if chat != -1001551357238:
+                    await event.client.send_message(chat, tol, file=file)
+                    lol += 1
+                elif chat == -1001551357238:
+                    pass
             except BaseException:
-                er += 1
-    await kk.edit(f"Done in {done} chats, error in {er} chat(s)")
-
-
-@bot.on(admin_cmd(pattern="gucast ?(.*)"))
-async def gucast(event):
-    if not event.out and not is_fullsudo(event.sender_id):
-        return await edit_or_reply(event, "`This Command Is Sudo Restricted.`")
-    xx = event.pattern_match.group(1)
-    if not xx:
-        return edit_or_reply(event, "`Give some text to Globally Broadcast`")
-    tt = event.text
-    msg = tt[7:]
-    kk = await edit_or_reply(event, "`Globally Broadcasting Msg...`")
-    er = 0
-    done = 0
-    async for x in bot.iter_dialogs():
-        if x.is_user and not x.entity.bot:
-            chat = x.id
-            try:
-                done += 1
-                await bot.send_message(chat, msg)
-            except BaseException:
-                er += 1
-    await kk.edit(f"Done in {done} chats, error in {er} chat(s)")
+                sed += 1
+    elif type == "-p":
+        async for krishna in event.client.iter_dialogs():
+            if krishna.is_user and not krishna.entity.bot:
+                chat = krishna.id
+                try:
+                    await event.client.send_message(chat, tol, file=file)
+                    lol += 1
+                except BaseException:
+                    sed += 1
+    elif type == "-g":
+        async for sweetie in event.client.iter_dialogs():
+            if sweetie.is_group:
+                chat = sweetie.id
+                try:
+                    if chat != -1001551357238:
+                        await event.client.send_message(chat, tol, file=file)
+                        lol += 1
+                    elif chat == -1001551357238:
+                        pass
+                except BaseException:
+                    sed += 1
+    else:
+        return await hol.edit(
+            "Please give a flag to Gcast message. \n\nAvailable flags are : \n• -all : To Gcast in all chats. \n• -pvt : To Gcast in private chats. \n• -grp : To Gcast in groups."
+        )
+    UwU = sed + lol
+    if type == "-a":
+        omk = "Chats"
+    elif type == "-p":
+        omk = "PM"
+    elif type == "-g":
+        omk = "Groups"
+    await hol.edit(
+        f"Gcast Executed Successfully !! \n\n Sent in : {lol} {omk}\n📍 Failed in : {sed} {omk}\n📍 Total :* {UwU} {omk}"
+    )
     
 CmdHelp("broadcast").add_command(
-   'gcast', None, 'Publish message to all channel and group'
-).add_command(
-  'gcust', None, 'Same as Gcast'
+   'gcast', "flag", 'Publish message to all channel and group'
 ).add()
